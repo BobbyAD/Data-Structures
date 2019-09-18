17f8551ca981e34f49b89ded82fc18d46ef1a752
Robert Driskell <driskell.ba@gmail.com>
2019-09-18 16:38:47 -0500

    finished binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,22 +1,64 @@
 import sys
-sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        insert_val = BinarySearchTree(value)
+        current_node = self
+        placed = False
+        while placed == False:
+            if value < current_node.value:
+                if current_node.left:
+                    current_node = current_node.left
+                else:
+                    current_node.left = insert_val
+                    placed = True
+            if value >= current_node.value:
+                if current_node.right:
+                    current_node = current_node.right
+                else:
+                    current_node.right = insert_val
+                    placed = True
 
-  def contains(self, target):
-    pass
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        current_node = self
+        found = False
+        end = False
+        while found == False and end == False:
+            if target < current_node.value:
+                if current_node.left:
+                    current_node = current_node.left
+                else:
+                    end = True
+            if target > current_node.value:
+                if current_node.right:
+                    current_node = current_node.right
+                else:
+                    end = True
+            if target == current_node.value:
+                found = True
+        return found
 
-  def for_each(self, cb):
-    pass
+    def get_max(self):
+        current_node = self
+        end = False
+        while end == False:
+            if current_node.right:
+                current_node = current_node.right
+            else:
+                end = True
+        return current_node.value
+
+    def for_each(self, cb):
+        if self.value == None:
+            return
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
